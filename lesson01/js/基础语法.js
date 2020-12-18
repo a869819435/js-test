c93087d4ba2caf872c8484fad8238f7df0d5bc47
a869819435 <869819435@qq.com>
2020-12-18 18:04:45 +0800

    Initial commit

---
+++ lesson01/js/基础语法.js
@@ -0,0 +1,13 @@
+// 1. 定义变量  变量类型(只有var表示全类型)  变量名 = 变量值
+var num = 1;
+alert(num);
+// 2. 条件控制
+var score = 1;
+if (score > 60 && score < 70){
+    alert(60-70);
+}else if (score > 70 && score < 80){
+    alert(70-80);
+}else{
+    alert("other");
+}
+
